b4b079173a8652508a44eec1238bab8a8cd81d54
Maria Fernanda Azolin <azolin.mf@gmail.com>
2024-04-09 20:54:47 -0300

    qr code pix funcionando

--- src/components/giftDetails/GiftDetails.js
+++ src/components/giftDetails/GiftDetails.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { QrCodePix } from 'qrcode-pix';
+import {useEffect, useState} from 'react'
 import styles from './GiftDetails.module.css';
-import CloseIcon from "../../assets/nav/closeIcon.svg";
+import CloseIcon from "../../assets/nav/close.svg";
 
 export const GiftDetails = ({ gift, onClose }) => {
+    const [qrCode, setQrCode] = useState('');
+    const [rawPix, setRawPix] = useState('');
+
+    useEffect(() => {
+        async function generateDynamicPix() {
+            const qrCodePix = QrCodePix({
+                version: '01',
+                key: '+5541998358181', //or any PIX key
+                name: 'Maria Fernanda Azolin',
+                city: 'CURITIBA',
+                message: 'Pagamento PIX',
+                cep: '82320120',
+                value: gift.value,
+            });
+
+            const rawPixStr = qrCodePix.payload();
+            const qrCodeBase64 = await qrCodePix.base64();
+
+            setRawPix(rawPixStr);
+            setQrCode(qrCodeBase64);
+        }
+
+        void generateDynamicPix();
+    }, [])
+
     return (
         <div className={styles.popup}>
             <div className={styles.popupContainer}>
                 <nav className={styles.navbar}>
-                    <img 
+                    <img
                         src={CloseIcon}
                         alt="close" 
                         onClick={onClose}
@@ -26,13 +53,28 @@ export const GiftDetails = ({ gift, onClose }) => {
                         }
                         <h2>{gift.name}</h2>
                     </div>
+                    <div>
+                        <h4> R$ {gift.value}</h4>
+                    </div>
 
                     <div className={styles.development}>
                         <h3>Fazer o PIX</h3>
                         <p> Valor do PIX??</p>
-                    </div>
 
+                        <img src={qrCode} alt={'QR Code PIX'}/>
+                        <p
+                            style={{
+                                maxWidth: '480px',
+                                margin: '0 auto',
+                                whiteSpace: 'nowrap',
+                                overflow: 'auto'
+                            }}
+                        >
+                            {rawPix}
+                        </p>
+                    </div>
                 </div>
+
             </div>
         </div>
     );
